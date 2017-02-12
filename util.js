c0f876947f3ece1347d3897d07ba2d9bce2a811c
Fabian Kästner <kruemel01@users.noreply.github.com>
2017-02-12 14:07:44 +0100

    Format logger output

--- util.js
+++ util.js
@@ -2,5 +2,8 @@ module.exports.logger = async function (ctx, next) {
     let receivedAt = Date.now();
     await next();
     let processingTime = Date.now() - receivedAt;
-    console.log(`Processing Time: ${processingTime}`);
+    let status = ctx.status === 200 ?
+        `${ctx.status} OK`.green
+       :`${ctx.status} ERROR`.red;
+    console.log(`${ctx.method} `.cyan + ctx.url.grey + " " + status + ` ${processingTime}ms`);
 }
